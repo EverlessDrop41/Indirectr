935c79a6ea60f156dd1983a2e77fe30ad1305367
emilyperegrine <peregrineemily@gmail.com>
2016-08-18 03:41:01 +0100

    Added indirect listing

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
 $(document).ready(function () {
-  var app = new Vue({
+  window.app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue.js!'
+      message: 'Hello Vue.js!',
+      editor: {
+        content: ""
+      },
+      indirects: [
+        { content: "Apple 1" },
+        { content: "Tomato 2" },
+        { content: "Grape 10" },
+        { content: "Pear 59" }
+      ]
+    },
+    methods: {
+      add: function () {
+        //Do the add stuff
+      },
+      composeTweet: function (content) {
+        alert(content);
+      }
     }
   });
 });
